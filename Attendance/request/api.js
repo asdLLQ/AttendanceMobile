dc1939d33dd3344c4b27aa60fe35145b2aeff290
asdLLQ <40430256+asdLLQ@users.noreply.github.com>
2021-04-06 03:13:55 +0000

    Update api.js

--- Attendance/request/api.js
+++ Attendance/request/api.js
@@ -1,46 +1,144 @@
-import request from "./request.js"
+import store from "@/store/index.js";
 
+// API 请求根路径
+var root = "http://127.0.0.1:8080"; //本地
+// var root = "http://119.23.34.173:8080"; //服务器
 
-const token = uni.getStorageSync('token');
-export default {
-	// 密码登录
-	passwordLogin(params) {
-		return request("/platform/metadata/logon", "GET", params)
-	},
+var system = "auth";
+var api = "api";
 
-	// 短信登录
-	noteLogin(params) {
-		return request("/platform/metadata/login", "GET", params)
-	},
-
-	// 获取验证码：用于短信登陆、密码找回、用户注销
-	getAuthCode(params) {
-		return request("/platform/metadata/sms", "GET", params)
-	},
-
-	// 获取注册账号验证码
-	getRegistAuthCode(params) {
-		return request("/platform/metadata/sms1", "GET", params)
-	},
-
-	// 登录者信息
-	getMyInfo(params) {
-		return request("/platform/metadata/bindinfo", "GET", params)
-	},
-
-	// 注册账号
-	registerAccount(params) {
-		return request("/platform/metadata/register", "GET", params)
-	},
-
-	// 找回密码
-	retrievePassword(params) {
-		return request("/platform/metadata/back", "GET", params)
-	},
-
-	// 注销账号
-	offAccount(params) {
-		return request("/platform/metadata/cancel", "GET", params)
-	},
+// API url路径
+var urls = {
+	login: `${root}/${system}/login`, //系统：登录授权
 	
+}
+
+var searchResult = {}
+
+// 封装请求方法
+var req = {
+	request(url, data, method, success, fail) {
+		uni.request({
+			url: url,
+			data: data,
+			method: method,
+			header: {
+				'content-type': 'application/json',
+				'Authorization': '' //默认携带token，未登录时，token为''
+			},
+			success: (res) => {
+				console.log(res);
+				if (res.statusCode == 200) {
+					success(res);
+				} else if (res.statusCode == 401) {
+					uni.showToast({
+						icon: "none",
+						title: res.errMsg || "登录信息过期，请重新登录！"
+					})
+				} else {
+					// 打印错误提示
+					uni.showToast({
+						icon: "none",
+						title: res.data.message || "请求失败"
+					})
+				}
+			},
+			fail: (err) => {
+				console.log(method, url, "fail", err);
+				if (fail) fail(err); // 如果失败方法非空，执行失败方法
+			}
+		});
+	},
+	requestWithToken(url, token, data1, method, success, fail) {
+		uni.request({
+			url: url,
+			header: {
+				'content-type': 'application/x-www-form-urlencoded',
+				'Authorization': token //默认携带token，未登录时，token为''
+			},
+			data: data1,
+			method: method,
+			success: (res) => {
+				success(res);
+			},
+			fail: (err) => {
+				console.log(method, url, "fail");
+			}
+		});
+	},
+	requestWithToken1(url, token, data1, method, success, fail) {
+		uni.request({
+			url: url,
+			header: {
+				'content-type': 'application/json',
+				'Authorization': token //默认携带token，未登录时，token为''
+			},
+			data: data1,
+			method: method,
+			success: (res) => {
+				success(res);
+			},
+			fail: (err) => {
+				console.log(err)
+				console.log(method, url, "fail");
+				if (fail) fail(err); // 如果失败方法非空，执行失败方法
+			}
+		});
+	},
+	getChildrenWithToken(url, token, method, success, fail) {
+		uni.request({
+			url: url,
+			header: {
+				'Authorization': token
+			},
+			// data: data1,
+			method: method,
+			success: (res) => {
+				// console.log(res);
+				if (res.statusCode == 200) {
+					success(res);
+				} else {
+					// 打印错误提示
+					uni.showToast({
+						icon: "none",
+						title: res.errMsg || "请求失败"
+					})
+				}
+			},
+			fail: (err) => {
+				console.log(method, url, "fail");
+				if (fail) fail(err); // 如果失败方法非空，执行失败方法
+			}
+		});
+	},
+	get(url, data, success, fail) {
+		this.request(url, data, 'GET', success, fail);
+	},
+	getWithToken(url, token, data, success, fail) {
+		this.requestWithToken(url, token, data, 'GET', success, fail);
+	},
+	post(url, data, success, fail) {
+		this.request(url, data, 'POST', success, fail);
+	},
+	put(url, data, success, fail) {
+		this.request(url, data, 'PUT', success, fail);
+	},
+	del(url, data, success, fail) {
+		this.request(url, data, 'DELETE', success, fail);
+	},
+
+
+	// 退出登录
+	logout() {
+		var url = urls.logout;
+		var data = {};
+		store.commit("logout");
+	}
+}
+
+export default {
+	root,
+	urls,
+	req,
+	searchResult
 }
