b06b9d45ac36f56509acb700f4879453f75afc73
chenjr15 <chenjr15@live.cn>
2021-06-25 21:27:32 +0800

    ✨ Http 请求Promise 封装！

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import { myRequest } from './util/api.js'
+import http from './util/http'
 
 Vue.prototype.$myRequest = myRequest
 
+Vue.prototype.http = http;
 Vue.config.productionTip = false
 
 App.mpType = 'app'
---
+++ util/http.js
@@ -0,0 +1,99 @@
+//const BASE_URL = 'https://attendance.keepdev.top/api'
+const BASE_URL = 'http://172.17.169.27:8080'
+const NOAUTH_URL = ['/auth/login', '/', '/version', '/echo']
+const OVERRIDED_METHODS = ['PUT', 'DELETE', 'PATCH']
+/**
+ *  Http Promise 封装
+ */
+const http = {
+	request(method, url, data, header = {}) {
+		return new Promise((reslove, reject) => {
+			uni.request({
+				url, data, header,
+				method,
+				success: ({ data, statusCode, header }) => {
+					if (statusCode == 200) reslove(data)
+					else reject(data.message)
+				},
+				fail: (error) => {
+
+					reject(error)
+				}
+			})
+		})
+	},
+	get(url, data, header = {}) {
+		return this.request('GET', url, data, header)
+	},
+	post(url, data, header = {}) {
+		return this.request('POST', url, data, header)
+	},
+	put(url, data, header = {}) {
+		return this.request('PUT', url, data, header)
+	},
+	delete(url, data, header = {}) {
+		return this.request('DELETE', url, data, header)
+	},
+	patch(url, data, header = {}) {
+		return this.request('PATCH', url, data, header)
+	}
+
+}
+uni.addInterceptor('request', {
+	/**
+	 * 请求发出前
+	 * @param {*} args 
+	 */
+	invoke(args) {
+		const token = uni.getStorageSync('token')
+		let method = args.method.toUpperCase()
+		let header = {}
+		if (OVERRIDED_METHODS.includes(method)) {
+			// 方法覆写，用post模拟这些方法
+			header['X-HTTP-Method-Override'] = method
+			method = 'POST'
+		}
+		if (!args.url.startsWith('http')) {
+			// request 触发前拼接 url 
+			args.url = BASE_URL + args.url
+		}
+
+		const url = new URL(args.url)
+
+		if (!NOAUTH_URL.includes(url.pathname)) {
+			//默认携带token，未登录时，token为''
+			header['Authorization'] = 'Bearer ' + token
+		}
+		args.header = { ...header, ...args.header }
+		args.method = method
+
+
+	},
+	/**
+	 * 请求成功, 似乎不管状态码是什么都会请求这个
+	 * @param {*} args 
+	 */
+	success({ data, statusCode, header, errMsg }) {
+		if (statusCode != 200) {
+			uni.showModal({
+				content: data.message,
+				showCancel: false,
+			})
+		}
+	},
+	/**
+	 * 请求失败
+	 * @param {*} err 
+	 */
+	fail(err) {
+		console.log('interceptor-fail', err)
+	},
+	// /**
+	//  * 请求结束(成功和失败)
+	//  * @param {*} res 
+	//  */
+	// complete(res) {
+	// 	console.log('interceptor-complete', res)
+	// }
+})
+export default http;
